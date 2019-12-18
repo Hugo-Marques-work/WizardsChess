67431e9f6f7e2be30792c53d8c47dc5ef5a96cdd
Hugo-Marques-work <slbhugomarques3@hotmail.com>
2019-12-18 18:42:11 +0000

    Small change to server communicator

--- client/js/ServerCommunicator.js
+++ client/js/ServerCommunicator.js
@@ -1,16 +1,54 @@
 class ServerCommunicator {
     constructor() {
+        this.socket = new WebSocket("wss://javascript.info");        
+        this.socket.onopen = this.globalOnOpen(event);
+        this.socket.onclose = this.globalOnClose(event);
+        this.socket.onerror = this.globalOnError(event);
 
     }
 
-
+    login() {
+        
+    }
     //Add websockets
-    move() {
+    move() {   
+        /*To be removed*/     
         gameBridge.executeMove();
+        return;
+        /*Until here*/
+        //if(receivedMessage)
+        this.socket.onmessage= this.moveOnMessage(e);
+    }
+
+    moveOnMessage(event) {
+        let parser = new Parser(event.data);
+        let moveAccepted = false;
+        let word = parser.readString();
+        //Deal with word and string
+
+        //....
+
+        if(moveAccepted == true) {
+            gameBridge.executeMove();
+        }
+        else {
+            //MOVE IS NOT ACCEPTED BY SERVER BUT IS ACCEPTED BY CLIENT. CLIENT IS CORRUPTED!
+        }
+    }
+
+    globalOnOpen(event) {
+        console.log("SOCKET HAS OPENED");
+    }
+    globalOnClose(event) {
+        alert("Connection has closed");
+    }
+
+    globalOnError(event) {
+        alert("Error sendind message to server");
     }
     
     drop() {
-
+        this.socket.close();
     }
 
     draw() {
